fced5e27ca392e8099f9ed94bfdaf26a0439e171
Kam-dev <kamrynkay@gmail.com>
2022-09-10 15:17:07 -0600

    finished step: 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information  
+    // addition of two integers
+        int a = 18;
+        int b = 6;
+        int c = a + b;
+        Console.WriteLine(c);
+    
+        // subtraction of two integers
+        c = a - b;
+        Console.WriteLine(c);
+
+        // multiplication of two integers
+        c = a * b;
+        Console.WriteLine(c);
+
+        // division of two integers
+        c = a / b;
+        Console.WriteLine(c);    
