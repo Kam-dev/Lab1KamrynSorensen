708aada4dff3581c0b2c8d6d6167c03217080d53
Kam-dev <kamrynkay@gmail.com>
2022-09-10 15:31:13 -0600

    finished step: 2

--- Program.cs
+++ Program.cs
@@ -1,24 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information 
     // The call to the WorkWithIntegers method
-    WorkWithIntegers();
-    // The declaration and definition of the WorkWithIntegers method
+    // WorkWithIntegers();
+OrderPrecedence();
+
     void WorkWithIntegers()
     {
-        // addition of two integers
         int a = 18;
         int b = 6;
         int c = a + b;
         Console.WriteLine(c);
-    
-        // subtraction of two integers
+
+
+        // subtraction
         c = a - b;
         Console.WriteLine(c);
 
-        // multiplication of two integers
+        // multiplication
         c = a * b;
         Console.WriteLine(c);
 
-        // division of two integers
+        // division
         c = a / b;
-        Console.WriteLine(c);        
+        Console.WriteLine(c);
+    }
+
+    void OrderPrecedence()
+    {
+        int a = 5;
+        int b = 4;
+        int c = 2;
+        int d = a + b * c;
+        Console.WriteLine(d);
+
+        d = (a + b) * c;
+        Console.WriteLine(d);
+
+        d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+        Console.WriteLine(d);
+
+        int e = 7;
+        int f = 4;
+        int g = 3;
+        int h = (e + f) / g;
+        Console.WriteLine(h);
     }
